f56a571da450f749f165d1e38853951e9942d4dc
lingesh1174 <147793730+lingesh1174@users.noreply.github.com>
2025-02-12 12:57:43 +0530

    Add files via upload

---
+++ Assignment_operator.java
@@ -0,0 +1,11 @@
+public class Assignment_operator
+{
+	public static void main(String[] args) {
+		int a=28;
+		int b=13;
+		b+=a;
+		a+=20;
+		System.out.println(a);
+		System.out.println(b);
+	}
+}
