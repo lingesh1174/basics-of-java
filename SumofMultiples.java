fe542fbaef1d362c6d3e550f4a2869d4c06c3f9a
lingesh1174 <147793730+lingesh1174@users.noreply.github.com>
2025-02-17 23:16:32 +0530

    Add files via upload

---
+++ SumofMultiples.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class SumofMultiples
+{
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	    int a=sc.nextInt();
+	    int b=sc.nextInt();
+	    int sum=0;
+	    while(b>0){
+	        sum+=a;
+	        System.out.print(a+" + ");
+	        a=a*10+(a%10);
+	        b--;
+	    }
+		System.out.println(" = "+sum);
+		
+	}
+}
